9d3dafcb928330de72d07500189c6547ca537820
shihao.chen <shihao.chen@jiduauto.com>
2023-10-19 21:36:12 +0800

    feat: add webgpu demo - add life game rule

--- webgpu/index.ts
+++ webgpu/index.ts
@@ -74,7 +74,7 @@ const bootstrap = async () => {
     code: Shader,
   });
 
-  const GRID_SIZE = 32;
+  const GRID_SIZE = 96;
 
   const cellStateArray = new Uint32Array(GRID_SIZE * GRID_SIZE);
   const cellStateBuffers = [
@@ -90,8 +90,8 @@ const bootstrap = async () => {
     }),
   ];
 
-  for (let i = 0; i < cellStateArray.length; i += 3) {
-    cellStateArray[i] = 1;
+  for (let i = 0; i < cellStateArray.length; ++i) {
+    cellStateArray[i] = Math.random() > 0.6 ? 1 : 0;
   }
   device.queue.writeBuffer(cellStateBuffers[0], 0, cellStateArray);
 
@@ -116,16 +116,42 @@ const bootstrap = async () => {
       @group(0) @binding(1) var<storage> cellStateIn: array<u32>;
       @group(0) @binding(2) var<storage, read_write> cellStateOut: array<u32>;
       
+      fn cellActive(x: u32, y: u32) -> u32 {
+        return cellStateIn[cellIndex(vec2(x, y))];
+      }
+      
       fn cellIndex(cell: vec2u) -> u32 {
-        return cell.y * u32(grid.x) + cell.x;
+        return (cell.y % u32(grid.y)) * u32(grid.x) + (cell.x % u32(grid.x));
       }
       
       @compute @workgroup_size(${WORKGROUP_SIZE}, ${WORKGROUP_SIZE})
       fn computeMain(@builtin(global_invocation_id) cell: vec3u){
-        if (cellStateIn[cellIndex(cell.xy)] == 1) {
-          cellStateOut[cellIndex(cell.xy)] = 0;
-        } else {
-          cellStateOut[cellIndex(cell.xy)] = 1;
+        let activeNeighbors = cellActive(cell.x + 1, cell.y + 1) +
+                        cellActive(cell.x + 1, cell.y) +
+                        cellActive(cell.x + 1, cell.y - 1) +
+                        cellActive(cell.x, cell.y - 1) +
+                        cellActive(cell.x - 1, cell.y - 1) +
+                        cellActive(cell.x - 1, cell.y) +
+                        cellActive(cell.x - 1, cell.y + 1) +
+                        cellActive(cell.x, cell.y + 1);
+        let i = cellIndex(cell.xy);
+        switch activeNeighbors {
+            case 1: {
+                // Active cells with 1 neighbors stay active.
+                cellStateOut[i] = cellStateIn[i];
+            }
+            case 2: {
+                // Active cells with 2 neighbors stay active.
+                cellStateOut[i] = cellStateIn[i];
+            }
+            case 3: {
+                // Cells with 3 neighbors become or stay active.
+                cellStateOut[i] = 1;
+            }
+            default: {
+                // Cells with < 1 or > 3 neighbors become inactive.
+                cellStateOut[i] = 0;
+            }
         }
       }
     `,
@@ -200,7 +226,7 @@ const bootstrap = async () => {
     },
   });
 
-  const UPDATE_INTERVAL = 500 as const;
+  const UPDATE_INTERVAL = 200 as const;
 
   let step = 0;
 
