61d162a7a89c8273040146135b71f864c838daba
shihao.chen <victor_csh@outlook.com>
2024-07-20 12:53:59 +0800

    feat: add polygon creator by three

--- threejs/index.ts
+++ threejs/index.ts
@@ -1,9 +1,7 @@
 import "../common/reload";
 import {
-  BoxGeometry,
   BufferGeometry,
-  Line,
-  LineBasicMaterial,
+  FrontSide,
   Mesh,
   MeshBasicMaterial,
   PerspectiveCamera,
@@ -12,70 +10,54 @@ import {
   WebGLRenderer,
 } from "three";
 
-const renderer = new WebGLRenderer();
-renderer.setSize(480, 480);
+const renderer = new WebGLRenderer({
+  antialias: true,
+});
+const size = 480;
+renderer.setSize(size * devicePixelRatio, size * devicePixelRatio);
+renderer.domElement.style.width = `${size}px`;
+renderer.domElement.style.height = `${size}px`;
 
 document.getElementById("canvas-wrapper")?.appendChild?.(renderer.domElement);
 
-const rafHandlers: Record<"cube", number> = { cube: 0 };
+const PolygonGeometry = (sides: number) => {
+  const geometry = new BufferGeometry();
 
-const cleanRenderTasks = () => {
-  Object.entries(rafHandlers).forEach(([key, handle]) => {
-    cancelAnimationFrame(handle);
-    rafHandlers[key as keyof typeof rafHandlers] = 0;
-  });
+  const points = [];
+  const vertices = new Array(sides - 2)
+    .fill(0)
+    .map((v, index) => [0, index + 1, index + 2])
+    .flat();
+
+  for (let pt = 0; pt < sides; pt++) {
+    const angle = Math.PI / 2 + (pt / sides) * 2 * Math.PI;
+    const x = Math.cos(angle);
+    const y = Math.sin(angle);
+    points.push(new Vector3(x, y, 0));
+  }
+  geometry.setFromPoints(points);
+  geometry.setIndex(vertices);
+
+  return geometry;
 };
 
-const bindCubeButton = () => {
+const renderPolygon = (sides: number) => {
+  renderer.clear();
+  const polygonGeometry = PolygonGeometry(sides);
+  const material = new MeshBasicMaterial({ color: "#33efdd", side: FrontSide });
+  const mesh = new Mesh(polygonGeometry, material);
   const scene = new Scene();
-  const camera = new PerspectiveCamera(75, 1, 0.1, 1000);
-  const geometry = new BoxGeometry(1, 1, 1);
-  const material = new MeshBasicMaterial({ color: 0x0000ff });
-  const cube = new Mesh(geometry, material);
-  scene.add(cube);
-  camera.position.z = 2;
-
-  const render = () => {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
-    rafHandlers.cube = requestAnimationFrame(render);
-  };
-
-  document.getElementById("cube-button")?.addEventListener?.("click", () => {
-    cleanRenderTasks();
-
-    render();
-  });
+  scene.add(mesh);
+  const camera = new PerspectiveCamera(45, 1, 1, 1000);
+  camera.position.set(0, 0, 3);
+  renderer.render(scene, camera);
 };
 
-const getLineWithOffset = (material: LineBasicMaterial, offset: number = 0) => {
-  const minifiedOffset = offset * 0.1;
-  const points = [
-    new Vector3(-10, 0 - minifiedOffset, 0),
-    new Vector3(0, 10 - minifiedOffset, 0),
-    new Vector3(10, 0 - minifiedOffset, 0),
-  ];
-  const geometry = new BufferGeometry().setFromPoints(points);
-  return new Line(geometry, material);
-};
+let sides = 3;
 
-const bindLineButton = () => {
-  const camera = new PerspectiveCamera(45, 1, 1, 500);
-  camera.position.set(0, 0, 50);
-  camera.lookAt(0, 0, 0);
-  const scene = new Scene();
-  const material = new LineBasicMaterial({ color: "#fff" });
-  scene.add(getLineWithOffset(material));
-  scene.add(getLineWithOffset(material, 1));
-  scene.add(getLineWithOffset(material, 2));
+renderPolygon(sides);
 
-  document.getElementById("line-button")?.addEventListener?.("click", () => {
-    cleanRenderTasks();
-
-    renderer.render(scene, camera);
-  });
-};
-
-bindCubeButton();
-bindLineButton();
+renderer.domElement.addEventListener("wheel", (event) => {
+  sides = Math.max(event.deltaY > 0 ? --sides : ++sides, 3);
+  renderPolygon(sides);
+});
